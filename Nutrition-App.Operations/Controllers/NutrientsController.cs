ba687c3da9e30c84025337592911f45123c9b62a
Ethan Chmielowiec <chmieloe@sheridancollege.ca>
2025-11-06 17:19:44 -0500

    view nutrients, css, main page

---
+++ Nutrition-App.Operations/Controllers/NutrientsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nutrition_App.Data;
+using Nutrition_App.Operations.Models.Nutrients;
+
+namespace Nutrition_App.Operations.Controllers
+{
+    public class NutrientsController : Controller
+    {
+        private readonly FoodDbContext _context;
+        public NutrientsController(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult View(string foodDescription)
+        {
+            var food = _context.Foods
+                .Include(f => f.FoodNutrients)
+                .ThenInclude(fn => fn.Nutrient)
+                .Include(f => f.FoodPortions)
+                .FirstOrDefault(f => f.Description.ToLower().Contains(foodDescription.ToLower()));
+                
+
+            var model = new NutrientViewModel
+            {
+                FoodDescription = foodDescription,
+                Food = food
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Nutrition-App.Operations/Models/Nutrients/NutrientViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Nutrition_App.Entities;
+
+namespace Nutrition_App.Operations.Models.Nutrients
+{
+    public class NutrientViewModel
+    {
+        public string FoodDescription { get; set; }
+        public Food Food { get; set; }
+    }
+}
