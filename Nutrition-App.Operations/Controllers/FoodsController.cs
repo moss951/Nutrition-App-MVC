b2b6508a37e7c82d90e13a8164e687a90365a958
Ethan Chmielowiec <chmieloe@sheridancollege.ca>
2025-11-06 15:40:52 -0500

    food search

---
+++ Nutrition-App.Operations/Controllers/FoodsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nutrition_App.Data;
+using Nutrition_App.Operations.Models.Foods;
+
+namespace Nutrition_App.Operations.Controllers
+{
+    public class FoodsController : Controller
+    {
+        private readonly FoodDbContext _context;
+        public FoodsController(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View(new FoodSearchViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Search(FoodSearchViewModel model)
+        {
+            model.QueriedFood = _context.Foods
+                .Where(f => f.Description.ToLower().Contains(model.FoodName.ToLower()))
+                .ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ Nutrition-App.Operations/Models/Foods/FoodSearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Nutrition_App.Entities;
+namespace Nutrition_App.Operations.Models.Foods
+{
+    public class FoodSearchViewModel
+    {
+        public string FoodName { get; set; }
+        public List<Food> QueriedFood {  get; set; }
+    }
+}
