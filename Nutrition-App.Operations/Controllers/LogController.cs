3531e3c6c733cd2ff7852c5122356a08c5f1d6de
ubiqwit <smalhotra.97@gmail.com>
2025-11-12 12:26:55 -0500

    Added log controller and add food to log button on search page - logic is still incomplete

---
+++ Nutrition-App.Operations/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nutrition_App.Services;
+using Nutrition_App.Entities;
+
+namespace Nutrition_App.Operations.Controllers
+{
+    public class LogController : Controller
+    {
+        private IFoodServices _foodServices;
+        private IUserServices _userServices;
+
+        public LogController(IFoodServices foodServices)
+        {
+            _foodServices = foodServices;
+        }
+
+
+        // Incomplete 
+        public IActionResult AddFoodToLog(int id)
+        {
+            Food food = _foodServices.GetFoodById(id);
+            return Ok();
+        }
+    }
+}
