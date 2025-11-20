2fb2181e31ee57657147504424834d554544307d
ubiqwit <smalhotra.97@gmail.com>
2025-11-20 18:36:05 -0500

    commit needed to sync properly

---
+++ Nutrition-App.Operations/Models/User/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Nutrition_App.Operations.Models.User
+{
+    public class LoginViewModel
+    {
+        public bool IsLoggedIn { get; set; } = false;
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Nutrition-App.Operations/Models/User/RegistrationViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Nutrition_App.Operations.Models.User
+{
+    public class RegistrationViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public string Sex { get; set; }
+        public double BMI { get; set; }
+        public bool IsCreated { get; set; }
+    }
+}
