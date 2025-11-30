bb93d347b2d26756e7622d83e24110a60ae88116
Andy Do <doandy@sheridancollege.ca>
2025-11-29 20:54:55 -0500

    Synchronized ID for test user

--- Nutrition-App.Entities/FoodEntities.cs
+++ Nutrition-App.Entities/FoodEntities.cs
@@ -94,44 +94,4 @@ namespace Nutrition_App.Entities
     {
         public List<Food> SurveyFoods { get; set; }
     }
-
-    /*
-     public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public double Height { get; set; }
-        public double Weight { get; set; }
-        public string Sex { get; set; }
-        public double BMI { get; set; }
-    }
-
-    public class DietGoals
-    {
-        public int Id { get; set; }
-        public double Calories { get; set; }
-        public double Protein { get; set; }
-        public double Carbohydrate { get; set; }
-        public double Fat { get; set; }
-        public double Fiber { get; set; }
-        public double Cholesterol { get; set; }
-        public double VitaminA { get; set; }
-        public double VitaminB2 { get; set; }
-        public double VitaminB3 { get; set; }
-        public double VitaminB6 { get; set; }
-        public double VitaminB7 { get; set; }
-        public double VitaminB9 { get; set; }
-        public double VitaminB12 { get; set; }
-        public double VitaminC { get; set; }
-        public double VitaminD { get; set; }
-        public double VitaminD { get; set; }
-        public double VitaminK { get; set; }
-        public double Sodium { get; set; }
-        public double Calcium { get; set; }
-        public double Iron { get; set; }
-        public double Magnesium { get; set; }
-        public double Potassium { get; set; }
-    }
-     */
 }
